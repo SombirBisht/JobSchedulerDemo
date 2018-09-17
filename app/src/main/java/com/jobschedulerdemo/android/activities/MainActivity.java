68bd734224ae718c0e9c47c8e1a08edcf60bb1a7
sombir bisht <bishtsom@gmail.com>
2018-09-17 15:35:23 +0530

    Job scheduling demo

--- app/src/main/java/com/jobschedulerdemo/android/activities/MainActivity.java
+++ app/src/main/java/com/jobschedulerdemo/android/activities/MainActivity.java
@@ -4,36 +4,68 @@ import android.annotation.TargetApi;
 import android.app.job.JobInfo;
 import android.app.job.JobScheduler;
 import android.content.ComponentName;
+import android.databinding.DataBindingUtil;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import com.jobschedulerdemo.android.R;
+import com.jobschedulerdemo.android.databinding.ActivityMainBinding;
 import com.jobschedulerdemo.android.services.MyJobService;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = MainActivity.class.getSimpleName();
+    private JobScheduler jobScheduler;
+    private ActivityMainBinding activityMainBinding;
 
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        // job scheduler
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        // start job button click
+        activityMainBinding.btnStartJob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //start your scheduled job
+                startJob();
+            }
+        });
+
+        //stop job button click
+        activityMainBinding.btnCancelJob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // stop your scheduled job
+                stopJob();
+            }
+        });
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public void startJob() {
+
+        jobScheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
         ComponentName componentName = new ComponentName(this, MyJobService.class);
         JobInfo jobInfo = new JobInfo.Builder(12, componentName)
                 .setRequiresCharging(true)
+                //.setPeriodic(5000)
                 .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
                 .build();
 
-        JobScheduler jobScheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
-        int resultCode = jobScheduler.schedule(jobInfo);
-        if (resultCode == JobScheduler.RESULT_SUCCESS) {
-            Log.d(TAG, "Job scheduled!");
-        } else {
-            Log.d(TAG, "Job not scheduled");
+        if (jobScheduler != null) {
+            jobScheduler.schedule(jobInfo);
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public void stopJob() {
+        if (jobScheduler != null) {
+            jobScheduler.cancelAll();
+            jobScheduler = null;
+            Toast.makeText(this, getResources().getString(R.string.jobCancelled), Toast.LENGTH_SHORT).show();
         }
     }
 }
--- app/src/main/java/com/jobschedulerdemo/android/services/MyJobService.java
+++ app/src/main/java/com/jobschedulerdemo/android/services/MyJobService.java
@@ -1,61 +1,77 @@
 package com.jobschedulerdemo.android.services;
 
+import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
+import android.app.ProgressDialog;
 import android.app.job.JobParameters;
 import android.app.job.JobService;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.util.Log;
+import android.view.WindowManager;
 
 @TargetApi(Build.VERSION_CODES.LOLLIPOP)
 public class MyJobService extends JobService {
     private static final String TAG = MyJobService.class.getSimpleName();
-    boolean isWorking = false;
-    boolean jobCancelled = false;
 
     // Called by the Android system when it's time to run the job
     @Override
     public boolean onStartJob(JobParameters jobParameters) {
         Log.e(TAG, "Job started!");
-        isWorking = true;
-        // We need 'jobParameters' so we can call 'jobFinished'
-        startWorkOnNewThread(jobParameters); // Services do NOT run on a separate thread
 
-        return isWorking;
-    }
+        AsyncTaskRunner asyncTaskRunner = new AsyncTaskRunner(getApplicationContext(), jobParameters);
+        asyncTaskRunner.execute();
 
-    private void startWorkOnNewThread(final JobParameters jobParameters) {
-        new Thread(new Runnable() {
-            public void run() {
-                doWork(jobParameters);
-            }
-        }).start();
-    }
-
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    private void doWork(JobParameters jobParameters) {
-        // 10 seconds of 'working' (1000*10ms)
-        for (int i = 0; i < 1000; i++) {
-            // If the job has been cancelled, stop working; the job will be rescheduled.
-            if (jobCancelled)
-                return;
-
-            try { Thread.sleep(10); } catch (Exception e) { }
-        }
-
-        Log.e(TAG, "Job finished!");
-        isWorking = false;
-        boolean needsReschedule = false;
-        jobFinished(jobParameters, needsReschedule);
+        return true;
     }
 
     // Called if the job was cancelled before being finished
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     public boolean onStopJob(JobParameters jobParameters) {
-        Log.e(TAG, "Job cancelled before being completed.");
-        jobCancelled = true;
-        boolean needsReschedule = isWorking;
-        jobFinished(jobParameters, needsReschedule);
-        return needsReschedule;
+        jobFinished(jobParameters, true);
+        return false;
+    }
+
+    /** Async task for background task */
+    @SuppressLint("StaticFieldLeak")
+    private class AsyncTaskRunner extends AsyncTask<Integer, Integer, String> {
+        ProgressDialog progressDialog;
+        private Context mContext;
+        private JobParameters mJobParameters;
+
+        private AsyncTaskRunner(Context context, JobParameters jobParameters) {
+            super();
+            mContext = context;
+            mJobParameters = jobParameters;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            progressDialog = new ProgressDialog(mContext);
+            progressDialog.setMessage("Wait for 10 seconds");
+            progressDialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
+            progressDialog.setCancelable(false);
+            progressDialog.show();
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            // execution of result of Long time consuming operation
+            progressDialog.dismiss();
+        }
+
+        @Override
+        protected String doInBackground(Integer... params) {
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            jobFinished(mJobParameters, false);
+            return "executed";
+        }
     }
 }
